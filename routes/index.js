7a07b0c41d28a9ce3ab4812ddc73de9fff7a7bdd
Josh Teperman <joshteperman@gmail.com>
2019-07-09 09:22:53 +1000

    added users/profile users/dashboard courses/ courses/course/:id courses/lesson/:id endpoints

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ router.use(express.json())
 router.use('/auth', require('./auth-routes'))
 router.use('/', require('./public-routes'))
 
+// protected endpoints ->
+
+router.use('/user', require('./user-routes'))
+router.use('/courses', require('./courses-routes'))
+
 module.exports = router;
