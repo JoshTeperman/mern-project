4c17eea6d20b606df0ebb57569b3387227887902
Josh Teperman <joshteperman@gmail.com>
2019-07-10 08:34:27 +1000

    dev deployment, but need to solve for db_uri loaded from .env file

--- server.js
+++ server.js
@@ -6,27 +6,28 @@ require('dotenv').config()
 const PORT = process.env.PORT || 5000
 
 // DEV DB on localhost -->
-// mongoose.connect(process.env.MONGO_PROD_URI, { useNewUrlParser: true }, err => {
-//   if (err) {
-//     console.log('👺  Error connecting to MongoDB');
-//   } else {
-//     console.log('✅  Connected to MongoDB');
-//   }
-// })
+// mongoose.connect(process.env.MONGO_DEV_URI, { useNewUrlParser: true }, err => {
+mongoose.connect('mongodb://localhost:27017/mi-academy', { useNewUrlParser: true }, err => {
+  if (err) {
+    console.log('👺  Error connecting to MongoDB');
+  } else {
+    console.log('✅  Connected to MongoDB');
+  }
+})
 
 // PROD DB on Atlas -->
-const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(process.env.MONGO_PROD_URI, { useNewUrlParser: true });
-client.connect(err => {
-  if (err) {
-    console.log('👺  Error connecting to mongoDB');
-  } else {
-    console.log('✅  Connected to mongoDB');
-  }
-  const collection = client.db("CA-MERN").collection("users");
-  // collection.insertOne({ email: 'josh@josh.com', password: 'password'})
-  client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// const client = new MongoClient(process.env.MONGO_PROD_URI, { useNewUrlParser: true });
+// client.connect(err => {
+//   if (err) {
+//     console.log('👺  Error connecting to mongoDB');
+//   } else {
+//     console.log('✅  Connected to mongoDB');
+//   }
+//   const collection = client.db("CA-MERN").collection("users");
+//   // collection.insertOne({ email: 'josh@josh.com', password: 'password'})
+//   client.close();
+// });
 
 app.use(require('./routes'))
 
