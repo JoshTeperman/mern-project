6fe2e261c993b79e0afc0bbd6740d84c3a4d4778
Josh Teperman <joshteperman@gmail.com>
2019-07-10 16:18:29 +1000

    backend code for login and registering users - need to connect to frontend

--- models/User.js
+++ models/User.js
@@ -1,35 +1,44 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;)
+const Schema = mongoose.Schema;
 
 
-const lessonSchema = new Schema({
-  _id: Schema.Types.ObjectId,
-  name: String,
-  description: String,
-  status: 'incomplete'
-})
+// const lessonSchema = new Schema({
+//   _id: Schema.Types.ObjectId,
+//   name: String,
+//   description: String,
+//   status: 'incomplete'
+// })
 
-const courseSchema = new Schema({
-  _id: Schema.Types.ObjectId,
-  name: String,
-  description: String,
-  category: String,
-  lessons: [{ type: Schema.Types.ObjectId, ref: 'Lesson' }],
-  status: 'incomplete'
-})
+// const courseSchema = new Schema({
+//   _id: Schema.Types.ObjectId,
+//   name: String,
+//   description: String,
+//   category: String,
+//   lessons: [{ type: Schema.Types.ObjectId, ref: 'Lesson' }],
+//   status: 'incomplete'
+// })
 
 const userSchema = new Schema({
-  email: String,
-  password: String,
-  role: String,
-  company: String,
-  status: String, // active, inactive
-  assignedCourses: {
-    Courses: [{ type: Schema.Types.ObjectId, ref: 'Course' }],
-    Completed: [{ type: Schema.Types.ObjectId, ref: 'Course' }]
+  email: {
+    type: String,
+    required: true
   },
-  history: {
-    Courses: []
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  company: {
+    type: String,
+    required: true
+  },
+  status: {
+    // active, inactive
+    type: String,
+    required: true
   }
 })
 
