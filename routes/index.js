9bd65c79333d70499d0b86a83a9fcd7a44385a6c
Josh Teperman <joshteperman@gmail.com>
2019-07-10 16:23:50 +1000

    added cors

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router();
+const cors = require('cors')
 
 router.use(express.json())
+router.use(cors())
 router.use('/auth', require('./auth-routes'))
 router.use('/', require('./public-routes'))
 
