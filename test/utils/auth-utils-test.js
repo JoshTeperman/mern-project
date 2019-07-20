28cbc60e5e90710474d64ca6840ac7d0bce1fbc3
Josh Teperman <joshteperman@gmail.com>
2019-07-21 08:48:00 +1000

    tests for auth-utils methods

---
+++ test/utils/auth-utils-test.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+const { assert } = require('chai')
+const { User } = require('../../models/User')
+const { generateHashedPassword, generateToken, checkPassword } = require('../../utils/auth-utils')
+const { createUser } = require('../../utils/User-utils')
+const path = require('path');
+const dotEnvPath = path.resolve('./.env')
+require('dotenv').config({ path: dotEnvPath});
+
+
+describe('Auth Utils Methods', () => {
+  before(async() => {
+    const mongoDB = "mongodb://127.0.0.1/mi-academy_testdb";
+    mongoose.connect(mongoDB, { useNewUrlParser: true });
+    await User.deleteMany();
+  })
+
+  after(async() => {
+    await mongoose.connection.close()
+  })
+
+  it('generateHashedPassword generates a hashed password', () => {
+    const hash = generateHashedPassword('password')
+    assert.ok(hash)
+  })
+
+  it('generateToken returns a token', () => {
+    const token = generateToken('email@email.com')
+    assert.ok(token)
+  })
+
+  describe('Check Password method', async () => {
+    
+    // const user = await createUser({ _id: new mongoose.Types.ObjectId().toString(), email: 'test@test.com', password: 'password', role: 'student'})
+    // console.log(user);
+    // const user = User.findOne('')
+    const user = {
+      role: 'student',
+      active: true,
+      programs: [],
+      _id: '5d3398fc74afd4d814e13899',
+      email: 'test@test.com',
+      password: '$2b$10$dw0t6wySyXbF.bNJrBJLqOl4MwBrOh9SsegMLwRVQtiOHZ/IAX5zO',
+      __v: 0
+    }
+
+    it('checkPassword returns true for a valid password', async () => {
+      const check = await checkPassword('password', user.password)
+
+      assert.ok(check)
+    })
+  
+    it('checkPassword returns false for an invalid password', async () => {
+      const check = await checkPassword('wrongpassword', user.password)
+
+      assert.isNotOk(check)
+    })
+  })
+})
